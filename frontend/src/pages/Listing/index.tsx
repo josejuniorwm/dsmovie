367cfd693279a1981f5bd999ea3885d317d5f910
Zé Júnior <josejuniorwm@gmail.com>
2022-03-10 21:23:02 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,32 @@
-function Listing(){
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Navbar/Pagination";
 
-    return(
-        <h1>Página de listagem</h1>
+function Listing() {
+
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+               <div className="row">
+                   <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                       <MovieCard />
+                  </div>
+                 
+                   <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                       <MovieCard />
+                  </div>
+                 
+                   <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                       <MovieCard />
+                  </div>
+                 
+                   <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                       <MovieCard />
+                  </div>
+                </div>        
+            </div>
+        </>
     )
 }
 
